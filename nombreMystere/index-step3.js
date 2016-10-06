3b795407bcd76c8fa14b5e767bd4c6ed5d305367
cphil31 <cecile_philippe@live.fr>
2016-10-06 12:22:16 +0200

    travaux sur le step 3

--- nombreMystere/index-step3.js
+++ nombreMystere/index-step3.js
@@ -1,19 +1,33 @@
 $(document).ready(main);
 
 // Fonction principale
-function main(){
-
+		function main(){
 	// Créer et initialiser une variable 'globale' qui 
 	// va stocker le nombre de tentatives restantes.
-
+		var tentatives=$(vies).val ();
 	// Créer et initialiser une variable 'globale' qui 
 	// va stocker le nombre "aléatoire" mystère.
+	function mystere(){
+		Math.round(Math.random() * (9  + 1));
+	}
+		var globale= $(mystere).val();
+	
+
+
+	$("#valider").click(clickValider);
+	console.log(mystere);
 
 
 	// == Fonction clickValider == 
 	// Créer la fonction 'clickValider'
+		function clickValider(){
+	console.log("valider");
+			}
 
 		// Récupérer le contenu de mon input
+		var recupinput=$("#nom").val();
+		console.log("contenu récupéré");
+
 
 		// Comparer ce contenu au nombre mystère.
 			/*
@@ -21,10 +35,17 @@ function main(){
 				- afficher une alert Gagné
 				- démarrer une nouvelle partie
 			*/
+			if (recupinput===mystere){
+				alert("Gagné !!!");
+			}
+				console.log("le contenu est égal");
 
 			// Sinon s'il est supérieur
 			// Afficher une alert Perdu, votre nombre est trop grand
-
+			if (recupinput>mystere) {
+				alert("Perdu !!!");
+			}
+			console.log("le contenu est supérieur");
 			// Sinon s'il est inférieur
 			// Afficher une alert Perdu, votre nombre est trop petit
 
