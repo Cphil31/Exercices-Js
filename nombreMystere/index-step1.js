1a5d442b0f9ec8b58db932f91465ce65e3e38229
cphil31 <cecile_philippe@live.fr>
2016-10-06 11:33:50 +0200

    Exercice 2 fait

--- nombreMystere/index-step1.js
+++ nombreMystere/index-step1.js
@@ -1,27 +1,33 @@
-$(document).ready(main);
 
 // Fonction principale
-function main(){
 
 	// == Fonction clickValider == 
 	// Créer la fonction 'clickValider'
 
+function clickvalider(){
+	var deux= $("#input").val();
+			
+			
 		// Récupérer le contenu de mon input
-
+			
 		// Comparer ce contenu à un nombre que vous choisissez
 			// Si le contenu est égal
 			// Afficher une alert Gagné
-
+if (deux==2){
+	alert("Gagné");
+}
 			// Sinon,
 			// Afficher une alert Perdu
+else{
+ alert("perdu");
 
+}
+							};
 
+$("button").click(clickvalider);
 	// /Fin == Fonction clickValider == 
 
 
 
 	// Ecrire le code qui va détecter que le bouton 'valider' est cliqué.
 	// Et qui appelle la fonction 'clickValider' défini ci-dessus
-
-
-}
--- nombreMystere/index-step2.js
+++ nombreMystere/index-step2.js
@@ -1,32 +1,52 @@
 $(document).ready(pied);
-
 // Fonction principale
 function pied(){
 
 	// == Fonction clickValider == 
 	// Créer la fonction 'clickValider'
+function ClickValider (){
 
 		// Récupérer le contenu de mon input
 
+var recup= $("#name").val();
+console.log(recup);
 		// Stocker un nombre ENTIER généré "aléatoirement"
+		/**
+ 
+ * Retourne un nombre ENTIER au hasard entre min et max!
+ */
+
+   var random= Math.round(Math.random() * (9  + 1));
+
+		console.log(random);
+
+
 
 		// Comparer ce contenu à ce nombre
 			// Si le contenu est égal
 			// Afficher une alert Gagné
-
+			
+if (recup===random){
+alert("Gagné !!!");
+}
+			
 			// Sinon s'il est supérieur
 			// Afficher une alert Perdu, votre nombre est trop grand
+if (recup>random){
+alert("Perdu  !!!");
+}			
 
 			// Sinon s'il est inférieur
 			// Afficher une alert Perdu, votre nombre est trop petit
-			
-			
+		if (recup<random){
+alert("Perdu  !!!");
+}	
 	// /Fin == Fonction clickValider == 
 
-
+	}
 
 	// Ecrire le code qui va détecter que le bouton 'valider' est cliqué.
 	// Et qui appelle la fonction 'clickValider' défini ci-dessus
-
-
+	
+$("#valid").click(ClickValider);
 }
